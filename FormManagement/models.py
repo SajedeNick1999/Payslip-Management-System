c44c690758057246a4620ba5ad0d335432ab6b35
sajede <sajede.nicknadaf78@gmail.com>
2020-08-05 01:49:50 +0430

    Manager and Form functions

--- FormManagement/models.py
+++ FormManagement/models.py
@@ -6,14 +6,34 @@ from django.contrib.postgres.fields import ArrayField
 
 class Form(models.Model):
     ID 			  = models.UUIDField(primary_key=True,)
-    CompanyID 	  = models.ForeignKey('CompanyManagement.Company',on_delete=models.CASCADE,)
+    CompanyID 	  = models.ForeignKey('CompanyManagement.Company',on_delete=models.CASCADE,default=0)
     Field         =ArrayField(ArrayField(models.CharField(max_length=100)))
 
-    def Form_AddField(self):
-        pass
-    def Form_EditField(self):
-        pass
-    def Form_DeleteField(self):
-        pass
+    def Form_AddField(self,name,type):
+        fields=self.Field
+        a=name+":"+str(type)
+        fields.append(a)
+        self.Field=fields
+        self.save()
+
+    def Form_EditField(self,id,name):
+        fields=self.Field
+        splitfield=(fields[id]).split(":")
+        a=name+":"+splitfield[1]
+        fields[id]=a
+        self.Field=fields
+        self.save()
+
+    def Form_DeleteField(self,id):
+        fields=self.Field
+        del fields[id]
+        self.Field=fields
+        self.save()
+
     def Form_GetForm(self):
-        pass
+        formlist=[]
+        fields=self.Field
+        for field in fields:
+            splitfield=field.split(":")
+            formlist.append(splitfield)
+        return formlist
--- ManagerManagement/models.py
+++ ManagerManagement/models.py
@@ -1,37 +1,70 @@
 from django.db import models
 #from CompanyManagement.models import Company
-#from EmployeeManagement.models import Employee
+from EmployeeManagement.models import Employee
+from FormManagement.models import Form
+from PayslipManagement.models import Payslip
 # Create your models here.
 class Manager(models.Model):
-    EmployeeID      =models.ForeignKey('EmployeeManagement.Employee',on_delete=models.CASCADE,default=0,)
+    EmployeeID      =models.ForeignKey('EmployeeManagement.Employee',on_delete=models.CASCADE,)
 
-    def Manager_AddEmployeeByFile(self):
-        pass
-    def Manager_AddEmployeeManual(self):
-        pass
-    def Manager_DeleteEmployee(self):
-        pass
-    def Manager_EditEmployee(self):
-        pass
+# for phase 2
+    # def Manager_AddEmployeeByFile(self):
+    #     pass
+    # def Manager_AddEmployeeManual(self):
+    #     pass
+    # def Manager_DeleteEmployee(self):
+    #     pass
+    # def Manager_EditEmployee(self):
+    #     pass
     def Manager_ShowEmployee(self):
-        pass
-    def Manager_AddField(self):
-        pass
-    def Manager_EditField(self):
-        pass
-    def Manager_DeleteField(self):
-        pass
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        emps=Employee.objects.filter(CompanyID=obj.CompanyID)
+        return emps;
+
+    def Manager_AddField(self,name,type):
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        comForm=Form.objects.get(CompanyID=obj.CompanyID)
+        comForm.Form_AddField(name,type)
+
+    def Manager_EditField(self,id,name):
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        comForm=Form.objects.get(CompanyID=obj.CompanyID)
+        comForm.Form_EditField(id,name)
+
+    def Manager_DeleteField(self,id):
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        comForm=Form.objects.get(CompanyID=obj.CompanyID)
+        comForm.Form_DeleteField(id)
+
     def Manager_AddPayslipByFile(self):
         pass
-    def Manager_AddPayslipManual(self):
-        pass
-    def Manager_EditPayslip(self):
-        pass
-    def Manager_DeletePayslip(self):
-        pass
+
+    def Manager_AddPayslipManual(self,empId,date,data):
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        #LastModifiedDate
+        #PayslipID
+        #without using payslip functions
+        pay=Payslip(CompanyID=obj.CompanyID,Date=date,EmployeeID=empId,LastModifiedDate="time",Data=data)
+        p.save()
+
+    def Manager_EditPayslip(self,payId,empId,date,data):
+        #without using payslip functions
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        pay=Payslip(PayslipID=payId,CompanyID=obj.CompanyID,Date=date,EmployeeID=empId,LastModifiedDate="time",Data=data)
+        p.save()
+
+    def Manager_DeletePayslip(payId):
+        obj=Payslip.objects.get(PayslipID=payId)
+        obj.delete()
+
     def Manager_GetCompanyID(self):
-        pass
+        obj=Employee.objects.get(ID=self.EmployeeID)
+        return obj.CompanyID
+
     def Manager_ShowForm(self):
-        pass
+        form=Form.objects.get(CompanyID=self.Manager_GetCompanyID())
+        return form.Form_GetForm()
+
     def Manager_CheckPayslipField(self):
+        #don't know
         pass
