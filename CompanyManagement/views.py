7a705bc12230f22af134355e15d6651fec6381c8
sajede <sajede.nicknadaf78@gmail.com>
2020-08-06 01:56:49 +0430

    json_test

--- CompanyManagement/views.py
+++ CompanyManagement/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Company
 
 # Create your views here.
+def CompanyAdd_view(request,name):
+    obj=Company.objects.get(Name=name)
+    data={
+        'name':obj.Name,
+        'max':obj.MaxEmployee
+    }
+    return JsonResponse(data)
