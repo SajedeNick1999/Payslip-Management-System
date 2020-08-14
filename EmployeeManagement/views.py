2f27d1a16139c719378ca85ef84bde0bdb695891
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2020-08-14 21:19:57 +0430

    Company-handling in login

--- EmployeeManagement/views.py
+++ EmployeeManagement/views.py
@@ -3,18 +3,22 @@ from django.http import JsonResponse
 from ManagerManagement.models import Manager
 from EmployeeManagement.models import Employee
 
-def login_view(request,username,password):
+def login_view(request,username,password,company):
 	# username == personnelCode
 	# password == password
 	employee = Employee.objects.filter(PersonnelCode=username,Password=password).first()
 	if employee == None:
 		return JsonResponse({'Token':'-1','ID':'-1','status':404,'isManager':'-1'}) # user not found
 	else:
-		employee.Employee_SetToken()
-		manager = Manager.objects.filter(EmployeeID=employee).first()
-		isManager = -1
-		if manager != None:
-			isManager = 1
+		if company == employee.CompanyID.Name:
+			employee.Employee_SetToken()
+			manager = Manager.objects.filter(EmployeeID=employee).first()
+			isManager = -1
+			if manager != None:
+				isManager = 1
+			else:
+				isManager = 0
+			return JsonResponse({'Token':employee.Token,'ID':employee.ID,'status':200,'isManager':isManager})
 		else:
-			isManager = 0
-		return JsonResponse({'Token':employee.Token,'ID':employee.ID,'status':200,'isManager':isManager})
+			return JsonResponse({'Token':'-1','ID':'-1','status':404,'isManager':'-1'}) # user not found in that company
+
